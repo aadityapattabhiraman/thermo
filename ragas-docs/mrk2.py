f00bdbc4d7b7df40c0ee567839800f764c09c886
aadityapattabhiraman <154038229+aadityapattabhiraman@users.noreply.github.com>
2025-04-03 03:30:25 +0000

    mrk2.py

--- ragas-docs/mrk2.py
+++ ragas-docs/mrk2.py
@@ -1,1 +1,36 @@
 #!/home/akugyo/Programs/Python/ragas/bin/python3
+
+import os
+from langchain_openai import AzureChatOpenAI, AzureOpenAIEmbeddings
+from ragas.llms import LangchainLLMWrapper
+from ragas.embeddings import LangchainEmbeddingsWrapper
+from ragas import SingleTurnSample
+from ragas.metrics import AspectCritic
+
+
+
+model = AzureChatOpenAI(
+    azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
+    azure_deployment=os.environ["AZURE_OPENAI_DEPLOYMENT_NAME"],
+    openai_api_version=os.environ["AZURE_OPENAI_API_VERSION"],
+)
+
+evaluator_llm = LangchainLLMWrapper(model)
+
+embedding = AzureOpenAIEmbeddings(
+    azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
+    azure_deployment="VAF_TEXT_EMBEDDER",
+    openai_api_version=os.environ["AZURE_OPENAI_API_VERSION"],
+)
+
+evaluator_embedding = LangchainEmbeddingsWrapper(embedding)
+
+test_data = {
+    "user_input": "summarise given text\nThe company reported an 8% rise in Q3 2024, driven by strong performance in the Asian market. Sales in this region have significantly contributed to the overall growth. Analysts attribute this success to strategic marketing and product localization. The positive trend in the Asian market is expected to continue into the next quarter.",
+    "response": "The company experienced an 8% increase in Q3 2024, largely due to effective marketing strategies and product adaptation, with expectations of continued growth in the coming quarter.",
+}
+
+metric = AspectCritic(name="summary_accuracy",llm=evaluator_llm, definition="Verify if the summary is accurate.")
+test_data = SingleTurnSample(**test_data)
+
+print(metric.single_turn_score(test_data))
